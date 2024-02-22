7e4935b8ed05a1307ec999882b1ca88e32bbd4b6
Chandan Kumar -X (chandku5 - US Technology Resources Private Limited at Cisco) <chandku5@cisco.com>
2024-02-22 20:58:46 +0530

    api-gateway

--- job-listing/src/main/java/com/chandan/security/config/ApiSecurityConfig.java
+++ job-listing/src/main/java/com/chandan/security/config/ApiSecurityConfig.java
@@ -1,15 +1,12 @@
-package com.chandan.config;
+package com.chandan.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ApiSecurityConfig {
